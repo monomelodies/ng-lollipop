361e44c26b4afddbc388a4c774b94e4c52dd3282
Marijn Ophorst <marijn@monomelodies.nl>
2017-06-11 15:58:13 +0200

    to es6

--- src/convertValueToDate.js
+++ src/convertValueToDate.js
@@ -1,32 +1,26 @@
 
 "use strict";
 
-var app = angular.module('lollipop.convertValueToDate', []);
-
-module.exports = app.name;
-
-/**
- * Factory returning a method that converts an array of valid date parts (e.g.
- * [yyyy, mm, dd]) into a `Date` object by calling the constructor.
- *
- * @param Array
- * @return Date
- */
-app.factory('convertValueToDate', function () {
-
-    function toConsumableArray(arr) {
-        if (Array.isArray(arr)) {
-            for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
-                arr2[i] = arr[i];
-            }
-            return arr2;
-        } else {
-            return Array.from(arr);
-        }
-    };
-    
-    return function (value) {
-        return new (Function.prototype.bind.apply(Date, [null].concat(toConsumableArray(value))))();
-    };
+export default angular.module('lollipop.convertValueToDate', [])
+    /**
+     * Factory returning a method that converts an array of valid date parts (e.g.
+     * [yyyy, mm, dd]) into a `Date` object by calling the constructor.
+     *
+     * @param Array
+     * @return Date
+     */
+    .factory('convertValueToDate', () => value => new (Function.prototype.bind.apply(Date, [null].concat(toConsumableArray(value))))())
+    .name
 });
+
+function toConsumableArray(arr) {
+    if (Array.isArray(arr)) {
+        for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+            arr2[i] = arr[i];
+        }
+        return arr2;
+    } else {
+        return Array.from(arr);
+    }
+};
 
