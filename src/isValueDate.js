9ddcd99206b73c3f89f6b0287fffd7ce8bdb96f7
Marijn Ophorst <marijn@monomelodies.nl>
2017-06-11 16:01:27 +0200

    to es6

--- src/isValueDate.js
+++ src/isValueDate.js
@@ -1,42 +1,40 @@
 
 "use steict";
 
-var dateRegexes = require('./dateRegexes');
-var dateCallbacks = require('./dateCallbacks');
+import dateRegexes from './dateRegexes';
+import dateCallbacks from './dateCallbacks';
 
-var app = angular.module('lollipop.isValueDate', [dateRegexes, dateCallbacks]);
-
-module.exports = app.name;
-
-/**
- * Checks a given value to see if it is a date based on `dateRegexes` and
- * `dateCallbacks`. On success an array of parts is returned which can be passed
- * to `convertValueToDate`. On failure, returns undefined;
- *
- * @param value Any (string) value to check.
- * @return Array|undefined
- * @see dateRegexes:value
- * @see dateCallbacks:service
- * @see convertValueToDate:factory
- */
-app.factory('isValueDate', ['dateRegexes', 'dateCallbacks', function (dateRegexes, dateCallbacks) {
-    return function (value) {
-        var match = undefined;
-        for (var i = 0; i < dateRegexes.length; i++) {
-            if (match = dateRegexes[i].exec(value)) {
-                match.shift();
-                var returner = [];
-                for (var i = 0; i < match.length; i++) {
-                    if (i in dateCallbacks) {
-                        returner.push(dateCallbacks[i](match[i]));
-                    } else {
-                        returner.push(match[i]);
+export default angular.module('lollipop.isValueDate', [dateRegexes, dateCallbacks])
+    /**
+     * Checks a given value to see if it is a date based on `dateRegexes` and
+     * `dateCallbacks`. On success an array of parts is returned which can be passed
+     * to `convertValueToDate`. On failure, returns undefined;
+     *
+     * @param value Any (string) value to check.
+     * @return Array|undefined
+     * @see dateRegexes:value
+     * @see dateCallbacks:service
+     * @see convertValueToDate:factory
+     */
+    .factory('isValueDate', ['dateRegexes', 'dateCallbacks', (dateRegexes, dateCallbacks) => {
+        return function (value) {
+            let match = undefined;
+            for (let i = 0; i < dateRegexes.length; i++) {
+                if (match = dateRegexes[i].exec(value)) {
+                    match.shift();
+                    let returner = [];
+                    for (let j = 0; j < match.length; j++) {
+                        if (j in dateCallbacks) {
+                            returner.push(dateCallbacks[i](match[j]));
+                        } else {
+                            returner.push(match[j]);
+                        }
                     }
+                    return returner;
                 }
-                return returner;
             }
-        }
-        return undefined;
-    };
-}]);
+            return undefined;
+        };
+    }])
+    .name;
 
