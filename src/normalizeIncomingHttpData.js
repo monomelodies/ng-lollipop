2f42a2828bb76299e02ea2babe34ba6dbd6212eb
Marijn Ophorst <marijn@monomelodies.nl>
2017-06-11 16:04:17 +0200

    to es6

--- src/normalizeIncomingHttpData.js
+++ src/normalizeIncomingHttpData.js
@@ -1,21 +1,19 @@
 
 "use strict";
 
-var normalizeData = require('./normalizeData');
-var app = angular.module('lollipop.normalizeIncomingHttpData', [normalizeData]);
+import normalizeData from './normalizeData';
+import appendTransform from './appendTransform';
 
-var appendTransform = require('./appendTransform');
-
-module.exports = app.name;
-
-/**
- * Wrapper service that normalizes incoming HTTP data. You need to inject this
- * somewhere in your application (e.g. in `app.run(['normalizeIncomingHttpData',
- * function(noop) {}]);`).
- *
- * @see normalizeData:factory
- */
-app.service('normalizeIncomingHttpData', ['$http', 'normalizeData', function ($http, normalizeData) {
-    $http.defaults.transformResponse = appendTransform($http.defaults.transformResponse, normalizeData);
-}]);
+export default angular.module('lollipop.normalizeIncomingHttpData', [normalizeData])
+    /**
+     * Wrapper service that normalizes incoming HTTP data. You need to inject this
+     * somewhere in your application (e.g. in `app.run(['normalizeIncomingHttpData',
+     * function(noop) {}]);`).
+     *
+     * @see normalizeData:factory
+     */
+    .service('normalizeIncomingHttpData', ['$http', 'normalizeData', function ($http, normalizeData) {
+        $http.defaults.transformResponse = appendTransform($http.defaults.transformResponse, normalizeData);
+    }])
+    .name;
 
