6ea8da73f1f575aabd45c7f73d91e6aa35d947b6
Marijn Ophorst <marijn@monomelodies.nl>
2017-06-11 15:58:22 +0200

    to es6

--- src/dateCallbacks.js
+++ src/dateCallbacks.js
@@ -1,26 +1,22 @@
 
 "use strict";
 
-var app = angular.module('lollipop.dateCallbacks', []);
-
-module.exports = app.name;
-
-/**
- * Default date callbacks for transforming a regexed date back to something
- * Javascript can grock in a `Date` constructor.
- *
- * For each matched subpattern, the corresponding index is invoked (if set).
- * Hence, for the default regex pattern this changes the months to be zero
- * offset since that's how Javascript rolls.
- *
- * @param mixed Each transformer receives the submatch and must convert it to
- *  an integer which can be used in the `Date` constructor.
- * @return int Each transformer must return an integer.
- * @see lollipop.dateRegexes:value
- */
-app.service('dateCallbacks', function () {
-    this[1] = function (value) {
-        return value - 1;
-    };
-});
+export default angular.module('lollipop.dateCallbacks', [])
+    /**
+     * Default date callbacks for transforming a regexed date back to something
+     * Javascript can grock in a `Date` constructor.
+     *
+     * For each matched subpattern, the corresponding index is invoked (if set).
+     * Hence, for the default regex pattern this changes the months to be zero
+     * offset since that's how Javascript rolls.
+     *
+     * @param mixed Each transformer receives the submatch and must convert it to
+     *  an integer which can be used in the `Date` constructor.
+     * @return int Each transformer must return an integer.
+     * @see lollipop.dateRegexes:value
+     */
+    .service('dateCallbacks', function () {
+        this[1] = value => value - 1;
+    })
+    .name;
 
